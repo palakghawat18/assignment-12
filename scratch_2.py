1460cda4fc18d971e094f81f2098d22698c2689f
palakghawat18 <ghawatpalak09@gmail.com>
2025-10-03 08:16:57 +0530

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,10 @@
+x=float(input("enter first no : "))
+y=float(input("enter second no : "))
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+print('addition : ', add)
+print('substraction : ', sub)
+print('multiplication : ', mul)
+print('division : ', div)
